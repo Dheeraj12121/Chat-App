84115e889d8d2319d4f3b8b07953e6db4d393fef
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-10-25 18:42:38 +0530

    creating context app

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
-import Login from './components/Authentication/Login'
-import Signup from './components/Authentication/Signup'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "./components/Authentication/Login";
+import Signup from "./components/Authentication/Signup";
+import { useHistory } from "react-router-dom";
 const Homepage = () => {
+
+  const history = useHistory()
+
+  useEffect(() =>{
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if(user) history.push("/chats")
+  },[history])
+
   return (
     <Container maxW={"xl"} centerContent>
       <Box
@@ -23,19 +42,19 @@ const Homepage = () => {
         p={4}
         borderRadius={"lg"}
         borderWidth={"1px"}
-        color={'black'}
+        color={"black"}
       >
-        <Tabs variant="soft-rounded" >
-          <TabList mb={'1em'}>
-            <Tab width={'50%'}>Login</Tab>
-            <Tab width={'50%'}>Sing Up</Tab>
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sing Up</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
-              <Login/>
+              <Login />
             </TabPanel>
             <TabPanel>
-              <Signup/>
+              <Signup />
             </TabPanel>
           </TabPanels>
         </Tabs>
