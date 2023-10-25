819da4de35b6681b69aa5e075943f58b9cc42bc9
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-10-25 16:59:28 +0530

    addToGroup api created

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats, createGroupChat, renameGroup } = require("../controllers/chatControllers");
+const { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route('/').post(protect, accessChat)
 router.route('/').get(protect, fetchChats)
 router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, renameGroupp);
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").delete(protect, removeFromGroup);
-// router.route("/groupadd").post(protect, addToGroup);
+router.route("/groupadd").post(protect, addToGroup);
 
 
 
