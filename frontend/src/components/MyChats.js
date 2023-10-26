1373b6edee29c28f806d68f9cce84d6aac6de4c7
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-10-26 14:43:37 +0530

    gropu

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -70,6 +70,38 @@ const MyChats = () => {
           rightIcon={<AddIcon />}
         ></Button>
       </Box>
+
+      <Box
+        d="flex"
+        flexDir={"column"}
+        p={3}
+        bg={"#F8F8F8"}
+        w={"100%"}
+        h={"100%"}
+        borderRadius={"lg"}
+        overflow={"hidden"}
+      >
+        {chats ? <Stack overflowY={'scroll'}>
+
+          {chats.map((chat)=>(
+            <Box
+            onClick={() => setSelectedChat(chat)}
+            cursor={'pointer'}
+            bg={selectedChat === chat ? '#38B2AC' : '#E8E8E8'}
+            color={selectedChat === chat ? 'white' : "black"}
+            px={3}
+            py={2}
+            borderRadius={'lg'}
+            key={chat._id}
+            >
+
+              <Text>
+                {!chat.isGroupChat ? getSender(loggedUser, chat.users) : chat.chatName}
+              </Text>
+            </Box>
+          ))}
+        </Stack> : <ChatLoading />}
+      </Box>
     </Box>
   );
 };
