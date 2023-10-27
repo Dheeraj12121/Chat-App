9f412e8db655ac72d19a55709508a83e778a9e8b
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-10-28 00:26:41 +0530

    created del user function

--- frontend/src/components/miscellaneous/GroupChatModal.js
+++ frontend/src/components/miscellaneous/GroupChatModal.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   FormControl,
   Input,
@@ -13,7 +14,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { Children } from "react";
-import UserListItem from '../UserAvator/UserListItem'
+import UserListItem from "../UserAvator/UserListItem";
 
 const GroupChatModal = ({}) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,52 +29,53 @@ const GroupChatModal = ({}) => {
   const { user, chats, setChats } = ChatState();
 
   const handleSearch = async (query) => {
-    setsearch(query)
-    if(!query){
-        return
+    setsearch(query);
+    if (!query) {
+      return;
     }
 
     try {
-        setChatLoading(true)
+      setChatLoading(true);
 
-        const config = {
-            headers: {
-                Authorization : `Bearer ${user.token}`
-            }
-        }
-        const {data} = await axios.get(`/api/user?search=${search}`,config)
-        console.log(data);
-        setLoading(false)
-        setSearchResult(data);
-
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
     } catch (error) {
-        toast({
-            title:"Error Occured!",
-            description:'Failed to Load the Search Results',
-            status:"error",
-            duration:5000,
-            isClosable:true,
-            position:"bottom-left"
-        })
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
     }
   };
 
-  const handleSubmit = () => {}
+  const handleSubmit = (delUser) => {
+    setSelectedUsers(selectedUser.filter(sel => selectedUsers._id !==delUser._id))
+  };
 
   const handleGroup = (userToAdd) => {
-    if(selectedUsers.includes(userToAdd)){
+    if (selectedUsers.includes(userToAdd)) {
       toast({
-        title:"User already added",
-        status:'warning',
-        isClosable:true,
-        position:'top',
-      })
+        title: "User already added",
+        status: "warning",
+        isClosable: true,
+        position: "top",
+      });
       return;
     }
-    setselectedUsers([...selectedUsers,userToAdd ])
-    }
+    setselectedUsers([...selectedUsers, userToAdd]);
+  };
 
-  const handleDelete = () =>{}
+  const handleDelete = () => {};
 
   return (
     <>
@@ -107,16 +109,29 @@ const GroupChatModal = ({}) => {
                   onChange={(e) => handleSearch(e.target.value)}
                 />
               </FormControl>
-              
-              {selectedUsers.map(u =>(
-                <UserBadgeItem key={user._id} user={u}
-                handleFunction{() => handleDelete(u)}
-                />
-              ) )}
 
+              <Box>
+                {selectedUsers.map((u) => (
+                  <UserBadgeItem
+                    key={user._id}
+                    user={u}
+                    handleFunction={() => handleDelete(u)}
+                  />
+                ))}
+              </Box>
 
-              {loading ?<div>Loading</div>: (
-                searchResult?.slice(0,4).map((user) => <UserListItem key={user._id} user={user} handleFunction={() => handleGroup(user)}/>)
+              {loading ? (
+                <div>Loading</div>
+              ) : (
+                searchResult
+                  ?.slice(0, 4)
+                  .map((user) => (
+                    <UserListItem
+                      key={user._id}
+                      user={user}
+                      handleFunction={() => handleGroup(user)}
+                    />
+                  ))
               )}
             </ModalBody>
           </ModalBody>
